17482ef888dbad37bd5cd136865cb594fadf9905
Bo Young <boyoung3389@gmail.com>
2020-08-10 19:52:46 -0400

    first commit

---
+++ app/src/main/java/android/example/myfoodrecords/MainActivity.java
@@ -0,0 +1,38 @@
+package android.example.myfoodrecords;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MyPagerAdapter mFragmentAdapter;
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setupUi();
+    }
+
+    private void setupUi() {
+
+        // Get the ViewPager and apply the PagerAdapter
+        mFragmentAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mViewPager.setAdapter(mFragmentAdapter);
+
+        // link the tabLayout and the viewpager together
+        mTabLayout = (TabLayout) findViewById(R.id.tabs);
+        mTabLayout.setupWithViewPager(mViewPager);
+
+    }
+
+}
---
+++ app/src/main/java/android/example/myfoodrecords/MyPagerAdapter.java
@@ -0,0 +1,54 @@
+package android.example.myfoodrecords;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class MyPagerAdapter extends FragmentPagerAdapter {
+
+    public MyPagerAdapter(FragmentManager supportFragmentManager) {
+        super(supportFragmentManager, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+    }
+
+    // Returns the fragment to display for that page
+    @Override
+    public Fragment getItem(int position) {
+        switch(position) {
+            case 0:
+                return new SummaryFragment();
+
+            case 1:
+                return new FavoriteFragment();
+
+            case 2:
+                return new CalanderFragment();
+
+            default:
+                return null;
+        }
+    }
+
+    // Will be displayed as the tab's label
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch(position) {
+            case 0:
+                return "Summary";
+
+            case 1:
+                return "Favorite";
+
+            case 2:
+                return "Calander";
+
+            default:
+                return null;
+        }
+    }
+
+    // Returns total number of pages
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
