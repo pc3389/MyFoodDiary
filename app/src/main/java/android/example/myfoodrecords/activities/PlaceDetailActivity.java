7aa5d2b14e1457ab622767467ba4a7315286360c
Bo Young <boyoung3389@gmail.com>
2020-09-02 16:58:14 -0400

    PrivatePlace button to Menu

--- app/src/main/java/android/example/myfoodrecords/activities/PlaceDetailActivity.java
+++ app/src/main/java/android/example/myfoodrecords/activities/PlaceDetailActivity.java
@@ -1,14 +1,22 @@
 package android.example.myfoodrecords.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.example.myfoodrecords.R;
 import android.example.myfoodrecords.utils.RealmHelper;
 import android.example.myfoodrecords.model.PlaceModel;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import io.realm.Realm;
 
@@ -20,11 +28,10 @@ public class PlaceDetailActivity extends AppCompatActivity {
     private int placeId;
     private EditText nameEditText;
     private EditText addressEditText;
-    private Button saveButton;
-    private Button deleteButton;
     private int id = 0;
 
     private PlaceModel newPlaceModel = new PlaceModel();
+    private PlaceModel placeModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,19 +53,27 @@ public class PlaceDetailActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Place Detail");
         nameEditText = findViewById(R.id.place_detail_name_et);
         addressEditText = findViewById(R.id.place_detail_address_et);
-        saveButton = findViewById(R.id.place_save_button);
 
-        final PlaceModel placeModel = helper.retrievePlaceWithId(placeId);
+        placeModel = helper.retrievePlaceWithId(placeId);
         nameEditText.setText(placeModel.getPlaceName());
         addressEditText.setText(placeModel.getAddress());
         id = placeModel.getId();
-        if(id == 0) {
-            deleteButton.setVisibility(View.INVISIBLE);
-        }
+    }
 
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_edit, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.edit_menu_save) {
+            hideKeyboard();
+            nullCheck();
+            if (!nameEditText.getText().toString().equals("")) {
                 newPlaceModel.setPlaceName(nameEditText.getText().toString());
                 newPlaceModel.setAddress(addressEditText.getText().toString());
                 newPlaceModel.setId(id);
@@ -68,6 +83,22 @@ public class PlaceDetailActivity extends AppCompatActivity {
                 helper.insertPlace(newPlaceModel);
                 finish();
             }
-        });
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void hideKeyboard() {
+        Activity activity = PlaceDetailActivity.this;
+        InputMethodManager inputManager = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (activity.getCurrentFocus() != null && inputManager != null) {
+            inputManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+            inputManager.hideSoftInputFromInputMethod(activity.getCurrentFocus().getWindowToken(), 0);
+        }
+    }
+    private void nullCheck() {
+        if (nameEditText.getText().toString().equals("")) {
+            Toast.makeText(this, "Please enter the Food Name", Toast.LENGTH_SHORT).show();
+        }
     }
 }
